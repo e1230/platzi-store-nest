d1e5dd1dbf6972f068fed7a5d6793629c6029307
e1230 <embeltrans@correo.udistrital.edu.co>
2022-12-22 21:28:58 -0500

    get

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,23 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get('products/filter')
+  getProductFilter(): string {
+    return `soy un filter`;
+  }
+  @Get('products/:id')
+  getProduct(@Param('id') id:string): string {
+    return `product ${id}`;
+  }
+
+  @Get('products')
+  getProducts(@Query() params: any): string {
+    const {limit = 100, offset = 0, brand} = params;
+    return `products: limit: ${limit} offset: ${offset} brand: ${brand}`;
+  }
+  @Get('categories/:id/products/:productId')
+  getCategory(@Param('id') id:string, @Param('productId') productoId:string): string {
+    return `category ${id} and product ${productoId}`;
   }
 }
