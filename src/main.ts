7c727bba4e2f97d5c5213935ce60d3bcb8190b9f
e1230 <embeltrans@correo.udistrital.edu.co>
2022-12-24 13:45:06 -0500

    validaciones

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //eliminar campos extra que no esten el dto
+      forbidNonWhitelisted: true, //alertar que esta ingresando campos prohibidos
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
