7970b9a615729b7c83644012c60984e2fe983ab5
e1230 <embeltrans@correo.udistrital.edu.co>
2022-12-24 11:38:35 -0500

    manejo de errores

--- src/controllers/products/products.controller.ts
+++ src/controllers/products/products.controller.ts
@@ -6,19 +6,23 @@ import {
   HttpCode,
   HttpStatus,
   Param,
+  ParseIntPipe,
   Post,
   Put,
   Query,
 } from '@nestjs/common';
+import { ProductsService } from '../../services/products/products.service';
 
 @Controller('products')
 export class ProductsController {
+  constructor(private productsService: ProductsService) {}
   @Get()
   getProducts(@Query() params: any): {} {
     const { limit = 100, offset = 0, brand } = params;
-    return {
-      message: `products: limit: ${limit} offset: ${offset} brand: ${brand}`,
-    };
+    // return {
+    //   message: `products: limit: ${limit} offset: ${offset} brand: ${brand}`,
+    // };
+    return this.productsService.findAll();
   }
   @Get('filter')
   getProductFilter(): {} {
@@ -28,25 +32,23 @@ export class ProductsController {
   }
   @Get(':id')
   @HttpCode(HttpStatus.ACCEPTED)
-  getProduct(@Param('id') id: string): {} {
-    return {
-      message: `product ${id}`,
-    };
+  getProduct(@Param('id', ParseIntPipe) id: number): {} {
+    // return {
+    //   message: `product ${id}`,
+    // };
+    return this.productsService.findOne(id);
   }
   @Post()
-  create(@Body() payload: any): {} {
-    return { message: 'accion de crear', payload };
+  create(@Body() payload: any) {
+    // return { message: 'accion de crear', payload };
+    this.productsService.create(payload);
   }
   @Put(':id')
-  update(@Param('id') id: number, @Body() payload: any): {} {
-    return {
-      message: 'update',
-      id,
-      payload,
-    };
+  update(@Param('id', ParseIntPipe) id: number, @Body() payload: any): {} {
+    return this.productsService.update(id, payload);
   }
   @Delete(':id')
-  delete(@Param('id') id: number): {} {
-    return { id };
+  delete(@Param('id', ParseIntPipe) id: number): {} {
+    return this.productsService.delete(id);
   }
 }
--- src/services/products/products.service.ts
+++ src/services/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Product } from '../../entities/products/products.entity';
 @Injectable()
 export class ProductsService {
@@ -8,7 +8,7 @@ export class ProductsService {
       id: 1,
       name: 'Producto 1',
       description:
-        'Ullamco id irure exercitation aute culpa non ullamco duis aute proident ut nulla aliquip aute.',
+        'Ullamco id irure exercitvation aute culpa non ullamco duis aute proident ut nulla aliquip aute.',
       price: 122,
       stock: 0,
       image: '',
@@ -20,7 +20,11 @@ export class ProductsService {
   }
 
   findOne(id: number) {
-    return this.products.find((item) => item.id === id);
+    const product = this.products.find((item) => item.id === id);
+    if (!product) {
+      throw new NotFoundException(`Producto con id: ${id} no encontrado`);
+    }
+    return product;
   }
 
   create(payload: any) {
@@ -35,9 +39,10 @@ export class ProductsService {
   update(id: number, payload: any) {
     const productFound = this.findOne(id);
     let message = '';
-    if (productFound != undefined) {
-      this.products[productFound.id - 1] = {
-        id: id,
+    if (productFound) {
+      const index = this.products.findIndex((item) => item.id === id);
+      this.products[index] = {
+        ...productFound,
         ...payload,
       };
       message = 'Product updated';
@@ -50,8 +55,9 @@ export class ProductsService {
   delete(id: number) {
     const productFound = this.findOne(id);
     let message = '';
-    if (productFound != undefined) {
-      this.products.splice(productFound.id - 1, 1);
+    if (productFound) {
+      const index = this.products.findIndex((item) => item.id === id);
+      this.products.splice(index);
       message = 'product deleted';
     } else {
       message = 'product not found';
